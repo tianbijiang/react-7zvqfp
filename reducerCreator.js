4b06b6808083961c58e6e31b203e5aac2b77ec2a
tianbijiang <tianbi.jiang@gmail.com>
2019-11-21 15:21:58 -0600

    1 file modified

--- reducerCreator.js
+++ reducerCreator.js
@@ -1,19 +1,24 @@
+import head from 'lodash/head';
+
 const defaultState = {
     pdpAttribute: {},
     minipdpAttribute: [],
 };
 
 export default function (reducerFunction, sourceMiniPDP, reducerName) {
+    reducerFunction.reducerName = reducerName;    // TODO: #TJ test for withReducerSaga
     return (state, action) => {
         if (state === undefined) {
             return sourceMiniPDP ? defaultState.minipdpAttribute : defaultState.pdpAttribute;
         }
-        if (action.payload.sourceMiniPDP !== sourceMiniPDP) {
-          return state;
+
+        if (action.sourceMiniPDP !== sourceMiniPDP &&
+            (!action.payload || action.payload.sourceMiniPDP !== sourceMiniPDP)) {
+            return state;
         }
-        
+
         if (sourceMiniPDP && (action.sourceMiniPDP || (action.payload && action.payload.sourceMiniPDP))) {
-            const inputState = state[0]; // always first element. need to change the logic if we are showing multiple MiniPDP
+            const inputState = head(state); // always first element. need to change the logic if we are showing multiple MiniPDP
             const newMiniPDPState = reducerFunction(inputState, action);
             if (inputState !== newMiniPDPState) {
                 return [newMiniPDPState];
